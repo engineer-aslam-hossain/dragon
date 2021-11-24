df18c3ee574bb5be0f9a4ece779235af040ad3ad
shubhamku044 <shubhamku044@gmail.com>
2021-11-24 16:02:20 +0530

    mobile friendly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: 'jit',
+	purge: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+		maxWidth: {
+			114: '36rem',
+			72: '18rem',
+			28: '7rem',
+			80: '20rem',
+			64: '16rem',
+			52: '13rem',
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
